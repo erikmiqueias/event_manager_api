388564f46b5007ee97007d936b533bc79ee1ea51
erikmiqueias <erikmiqueiaspereira@outlook.com>
2025-07-02 16:10:49 -0400

    initial commit

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,10 +5,11 @@ config();
 
 // no caso só tem uma função de conexão
 const pool: pg.Pool = new pg.Pool({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  port: +process.env.POSTGRES_PORT!,
-  database: process.env.POSTGRES_DB,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
